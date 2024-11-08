7036aec0c920ae9fd9addf4a761edf3f7c5621a7
Taisiia Simakova <stipaxa@gmail.com>
2024-11-08 10:53:41 +0000

    Initial comment

--- smtp_email_project/urls.py
+++ smtp_email_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for my_project project.
+URL configuration for smtp_email_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
